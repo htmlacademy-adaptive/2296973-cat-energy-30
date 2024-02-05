7112ac013e5031eb8092c1c15a4ff5d09eea6357
fesko32 <fesko_il@icloud.com>
2024-02-05 13:03:28 +0300

    Правки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
 /* в этот файл добавляет скрипты*/
+const btnAct = document.querySelector('.main-nav__toggle');
+const nav = document.querySelector('.main-nav__checkbox');
+
+
+// eslint-disable-next-line no-undef
+nav.addEventListener('click', () => {
+  btnAct.classList.toggle ('main-nav__toggle--close');
+
+});
